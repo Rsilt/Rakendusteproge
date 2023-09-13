ad312bbdf36d14630c789cc1c3f26f3756c2d737
Roland <rsilt@tlu.ee>
2023-09-13 15:12:24 +0300

    2nd Lesson

--- lessons/06-09-23/basics.js
+++ lessons/06-09-23/basics.js
@@ -63,7 +63,57 @@ const numberArray = [1,2,3,4,5]
 
 //luua uus massiiv selle massiivi põhjal, kus igal numbrile on liidetud juurde 5.
 
-const  newArray = numberArray.map()
+//const  newArray = numberArray.map()
 //mapi sisse tuleb arrow function
 //.map() =>{})
-const newAddedArray = newNumberArray.map((n) => n + 5)
+const newAddedArray = newNumberArray.map((element) => element + 5)
+//element on array osa, map käib iga elemendi läbi annab callbacki ja siis votab jargmise elemendi
+const newArray = numberArray.map((element,index, array) => {
+    console.log({
+    element: element,
+    newElement: element +5,
+    "index": index,
+    array
+    })
+    return element +5
+}
+
+
+//filter kõik numbrid väiksemad kui 4 
+
+const filteredArray = numberArray.filter(element => element < 4)
+
+console.log({numberArray, filteredArray})
+
+
+// mapi kasutades teha Juku, vanus on juku nimetähtede arv+ 20, email on eesnimi väiksetähega+ @company.com,
+// aadress on eesnimi Street ja suvaline number ja username on nimi tagurpidi
+
+const names = ["Juku", "Mai", "Jaanus", "Anni"]
+
+const data = names.map(name => {
+    return {
+        name: name,
+        age: name.length + 20,
+        email: `${name.toLowerCase}@company.com`,
+        address: `${name} Street 35`,
+        username: name.split("").reverse().join("")
+    }
+})
+console.log(data)
+
+//tahan lisada juurde Juku pikkuse
+
+const newJuku ={
+    ...data[0],
+    height: 175
+}
+console.log({newJuku})
+
+//Tahan muuta juku vanust
+
+const evenNewerJuku={
+    ...NewJuku1,
+    age:27
+}
+console.log({newJuku, evenNewerJuku})
